5e848e6e8d2211154553fe720f3b38efb2ea31ff
William Roger <williamroger.frontend@gmail.com>
2019-03-02 02:18:12 -0300

    Definidas Variáveis e criados os componentes HeaderMenu e UserBox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
+import { UserBoxComponent } from './components/header-menu/user-box/user-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    UserBoxComponent
   ],
   imports: [
     BrowserModule
