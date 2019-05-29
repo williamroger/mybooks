7b2ba05356de9c792e049117cd8e5ac72a093c79
William Roger <williamroger.frontend@gmail.com>
2019-05-29 18:08:24 -0300

    Mais uma tentativa desesperada de fazer a comuncação Front/Back funcionar via POST

--- src/app/providers/biblioteca.service.ts
+++ src/app/providers/biblioteca.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MYBOOKS_API } from 'src/app/app.api';
 import { Livro } from '../components/livro/livro.model';
 
@@ -7,6 +7,15 @@ import { Livro } from '../components/livro/livro.model';
   providedIn: 'root'
 })
 export class BibliotecaService {
+  private _httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers':
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    })
+  };
 
   constructor(private http: HttpClient) { }
 
@@ -15,6 +24,7 @@ export class BibliotecaService {
   }
 
   insertLivro(livro: Livro) {
-    return this.http.post<any>(`${MYBOOKS_API}/biblioteca/adicionarlivro`, livro);
+    console.log(livro);
+    return this.http.post<any>(`${MYBOOKS_API}/biblioteca/adicionarlivro`, livro, this._httpOptions);
   }
 }
