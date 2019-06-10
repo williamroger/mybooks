afe180ced5f3cee7a557b4b1c7a39b1993f4be15
William Roger <williamroger.frontend@gmail.com>
2019-06-10 20:41:11 -0300

    Ajustes Guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -12,7 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-  ) {
+  ) : Observable<boolean> | boolean {
 
   }
 }
